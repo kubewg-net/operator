91ae2f370a3c6f8e5296298af26160358c4de4a3
Jacob McSwain <jacob@mcswain.dev>
2024-05-16 16:05:38 -0500

    fix e2e test calling make

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -67,7 +67,7 @@ var _ = Describe("controller", Ordered, func() {
 			var projectimage = "example.com/kubewg:v0.0.1"
 
 			By("building the manager(Operator) image")
-			cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectimage))
+			cmd := exec.Command("sh", "-c", "CGO_ENABLED=0 go build -o kubewg ./cmd/main.go && docker build -t "+projectimage+" .")
 			_, err = utils.Run(cmd)
 			ExpectWithOffset(1, err).NotTo(HaveOccurred())
 
