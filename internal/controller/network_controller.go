0ada3adaaedb2408f1b73969d6265f043dc68b06
Jacob McSwain <jacob@mcswain.dev>
2024-05-17 01:12:27 -0500

    controllers: log the reconcile

--- internal/controller/network_controller.go
+++ internal/controller/network_controller.go
@@ -44,15 +44,17 @@ type NetworkReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Network object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
-func (r *NetworkReconciler) Reconcile(ctx context.Context, _ ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+func (r *NetworkReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+	log.Info("Reconciling Network")
+
+	var network kubewgv1.Network
+	if err := r.Get(ctx, req.NamespacedName, &network); err != nil {
+		log.Error(err, "unable to fetch Network")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	log.Info("Network", "Network", network)
 
 	// TODO(user): your logic here
 
--- internal/controller/peer_controller.go
+++ internal/controller/peer_controller.go
@@ -43,15 +43,17 @@ type PeerReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Peer object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
-func (r *PeerReconciler) Reconcile(ctx context.Context, _ ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+func (r *PeerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+	log.Info("Reconciling Peer")
+
+	var peer kubewgv1.Peer
+	if err := r.Get(ctx, req.NamespacedName, &peer); err != nil {
+		log.Error(err, "unable to fetch Peer")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	log.Info("Peer", "Peer", peer)
 
 	// TODO(user): your logic here
 
